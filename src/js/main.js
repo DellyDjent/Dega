b12420e12275a9620713b714bc256e56658d770b
dellydjent <ilikedellydjent@gmail.com>
2017-04-13 07:06:48 +0300

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+jQuery(function($) {
+
+    $('ul.navbar-nav li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    });
+
+    $('#sliderBanner').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: false,
+        autoplay: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+    $('#mainSlider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
+});
